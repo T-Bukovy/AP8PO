7d565fee4b050f6c05b4107c0b3985aa61649e38
Tomas Bukovy <t_bukovy@utb.cz>
2023-04-11 12:22:11 +0200

    pridani portalu a pauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+
+    // Start is called before the first frame update
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        } 
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Ukoncuji se ");
+    }
+    public void LoadMainMenu()
+    {
+        Debug.Log("Nacítam hlavni menu");
+        SceneManager.LoadScene(0); //Index 0 je vždy úvodní obrazovka => v případě změny... změnit duh
+    }
+}
